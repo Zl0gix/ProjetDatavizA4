19e792b2a23ed30dcaaac783611d96e95a805d6c
Zl0gix <raph10richard@gmail.com>
2022-01-05 22:54:17 +0100

    [ADD] draft of parameter recognition in API

---
+++ README.md
@@ -0,0 +1,4 @@
+ESILV A4 - Python for data analysis project
+==
+
+On this repository you will find the work of ROUET Thibaut and RICHARD Raphaël around the dataset [Diabetes 130-US hospitals for years 1999-2008 Data Set](https://archive.ics.uci.edu/ml/datasets/diabetes+130-us+hospitals+for+years+1999-2008)
--- flaskr/prediction.py
+++ flaskr/prediction.py
@@ -1,7 +1,19 @@
-from flask import Blueprint
+from flaskr import BEST_MODEL
+from flask import Blueprint, request
+from joblib import load
 
 bp = Blueprint("prediction", __name__)
 
 @bp.route('/prediction')
 def prediction():
-    return "Hello World!"
+    if 'model' in request.args.keys():
+        model = load("Backend/models/"+request.args['model']+".joblib")
+    else:
+        model = load("Backend/models/"+BEST_MODEL+".joblib")
+
+    mandatory = [""]
+    for item in mandatory:
+        if item not in request.args.keys():
+            return ""
+
+    return str(list(request.args.keys()) + list(request.args.values()))
